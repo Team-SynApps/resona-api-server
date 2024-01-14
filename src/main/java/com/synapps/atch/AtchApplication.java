f4d2a91e9b33e977c687cb5d012b0b7892fde1bc
Brian <bwook9908@gmail.com>
2024-01-14 12:19:30 +0900

    first commit

---
+++ src/main/java/com/synapps/atch/AtchApplication.java
@@ -0,0 +1,13 @@
+package com.synapps.atch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/synapps/atch/AtchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.synapps.atch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
