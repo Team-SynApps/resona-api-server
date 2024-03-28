918b9350e7f1911c0a29f419cda757710eb62d07
Brian <bwook9908@gmail.com>
2024-03-28 12:59:45 +0900

    docs: getMember() 주석 추가

--- src/main/java/com/synapps/atch/mysql/member/service/MemberService.java
+++ src/main/java/com/synapps/atch/mysql/member/service/MemberService.java
@@ -2,7 +2,6 @@ package com.synapps.atch.mysql.member.service;
 
 import com.synapps.atch.mysql.member.entity.Member;
 import com.synapps.atch.mysql.member.repository.MemberRepository;
-import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.User;
@@ -13,6 +12,10 @@ import org.springframework.stereotype.Service;
 public class MemberService {
     private final MemberRepository memberRepository;
 
+    /**
+     * SecurityContextHolder에서 관리하는 context에서 userPrincipal을 받아옴
+     * @return 멤버를 이메일 기준으로 불러옴
+     */
     public Member getMember() {
         User userPrincipal = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
         return memberRepository.findByEmail(userPrincipal.getUsername());
