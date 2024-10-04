79138f474cc5035711d4660d8474d0cbbb028b9f
speculatingwook <bwook9908@gmail.com>
2024-10-04 13:42:37 +0900

    refactor: change response Entity in MailController

--- src/main/java/com/synapps/atch/external/email/MailController.java
+++ src/main/java/com/synapps/atch/external/email/MailController.java
@@ -1,21 +1,31 @@
 package com.synapps.atch.external.email;
 
+import com.synapps.atch.global.config.ServerInfoConfig;
+import com.synapps.atch.global.dto.MetaDataDto;
+import com.synapps.atch.global.dto.ResponseDto;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
+import java.util.List;
 
 @RestController
 @RequestMapping("/email")
 @RequiredArgsConstructor
 public class MailController {
     private final MailService mailService;
+    private final ServerInfoConfig serverInfo;
     private int number; // 이메일 인증 숫자를 저장하는 변수
+
+    private MetaDataDto createSuccessMetaData(String queryString){
+        return MetaDataDto.createSuccessMetaData(queryString, serverInfo.getApiVersion(), serverInfo.getServerName());
+    }
 
     // 인증 이메일 전송
     @PostMapping()
-    public HashMap<String, Object> mailSend(String mail) {
+    public ResponseEntity<?> sendMail(HttpServletRequest request, String mail) {
         HashMap<String, Object> map = new HashMap<>();
 
         try {
@@ -28,16 +38,20 @@ public class MailController {
             map.put("success", Boolean.FALSE);
             map.put("error", e.getMessage());
         }
+        MetaDataDto metaData = createSuccessMetaData(request.getQueryString());
+        ResponseDto responseData = new ResponseDto(metaData, List.of(map));
 
-        return map;
+        return ResponseEntity.ok(responseData);
     }
 
     // 인증번호 일치여부 확인
     @GetMapping()
-    public ResponseEntity<?> mailCheck(@RequestParam String userNumber) {
+    public ResponseEntity<?> mailCheck(HttpServletRequest request, @RequestParam String userNumber) {
 
         boolean isMatch = userNumber.equals(String.valueOf(number));
+        MetaDataDto metaData = createSuccessMetaData(request.getQueryString());
+        ResponseDto responseData = new ResponseDto(metaData, List.of(isMatch));
 
-        return ResponseEntity.ok(isMatch);
+        return ResponseEntity.ok(responseData);
     }
 }
--- src/main/java/com/synapps/atch/global/config/SwaggerConfig.java
+++ src/main/java/com/synapps/atch/global/config/SwaggerConfig.java
@@ -6,12 +6,8 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
-import io.swagger.v3.oas.models.servers.Server;
-import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.context.request.RequestContextHolder;
-import org.springframework.web.context.request.ServletRequestAttributes;
 
 import java.util.List;
 
