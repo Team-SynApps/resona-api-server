b2adde24aa9560507cdac2b9d9fb7b6ad9fbabad
speculatingwook <bwook9908@gmail.com>
2024-10-04 12:51:34 +0900

    fix: revert to original

--- src/main/java/com/synapps/atch/global/config/SwaggerConfig.java
+++ src/main/java/com/synapps/atch/global/config/SwaggerConfig.java
@@ -1,23 +1,23 @@
 package com.synapps.atch.global.config;
 
+
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
 import io.swagger.v3.oas.models.servers.Server;
-import org.springframework.beans.factory.annotation.Value;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
 
 import java.util.List;
 
+
 @Configuration
 public class SwaggerConfig {
-
-    @Value("${swagger.server.url:#{null}}")
-    private String swaggerServerUrl;
-
     @Bean
     public OpenAPI openAPI() {
         SecurityScheme securityScheme = getSecurityScheme();
@@ -29,15 +29,13 @@ public class SwaggerConfig {
                         .description("유저, sns 기본 기능들이 있습니다.")
                         .version("1.0.0"))
                 .components(new Components().addSecuritySchemes("bearerAuth", securityScheme))
-                .security(List.of(securityRequirement))
-                .servers(List.of(new Server().url(swaggerServerUrl)));
+                .security(List.of(securityRequirement));
     }
 
     private SecurityScheme getSecurityScheme() {
         return new SecurityScheme().type(SecurityScheme.Type.HTTP).scheme("bearer").bearerFormat("JWT")
                 .in(SecurityScheme.In.HEADER).name("Authorization");
     }
-
     private SecurityRequirement getSecurityRequireMent() {
         return new SecurityRequirement().addList("bearerAuth");
     }
