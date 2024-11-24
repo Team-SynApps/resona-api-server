04a5cf85d3b6cfc1767aac8ff146aa00c9bf6d72
speculatingwook <bwook9908@gmail.com>
2024-11-24 17:56:46 +0900

    refactor: add column names, add relationshipMapping

--- src/main/java/synapps/resona/api/mysql/member/entity/account/AccountInfo.java
+++ src/main/java/synapps/resona/api/mysql/member/entity/account/AccountInfo.java
@@ -5,6 +5,7 @@ import jakarta.validation.constraints.NotNull;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import synapps.resona.api.mysql.member.entity.Member;
 import synapps.resona.api.oauth.entity.ProviderType;
 import synapps.resona.api.oauth.entity.RoleType;
 
@@ -18,25 +19,35 @@ public class AccountInfo {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @OneToOne
+    @JoinColumn(name = "member_id") // 외래 키 컬럼 이름
+    private Member member;
+
     @Enumerated(EnumType.STRING)
+    @Column(name="role_type")
     @NotNull
     private RoleType roleType;
 
     @Enumerated(EnumType.STRING)
     @NotNull
+    @Column(name="provider_type")
     private ProviderType providerType;
 
     @Enumerated(EnumType.STRING)
     @NotNull
+    @Column(name="account_status")
     private AccountStatus status;
 
     @NotNull
+    @Column(name="last_accessed_at")
     private LocalDateTime lastAccessedAt;
 
     @NotNull
+    @Column(name="created_at")
     private LocalDateTime createdAt;
 
     @NotNull
+    @Column(name="updated_at")
     private LocalDateTime updatedAt;
 
     private AccountInfo(RoleType roleType, ProviderType providerType, AccountStatus status, LocalDateTime lastAccessedAt, LocalDateTime createdAt, LocalDateTime updatedAt) {
