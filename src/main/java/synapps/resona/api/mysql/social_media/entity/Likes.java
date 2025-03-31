74f096f3c9633476ce727c3bf38b04055a5713a2
speculatingwook <bwook9908@gmail.com>
2025-03-31 15:53:31 +0900

    fix: fix table name like to likes

--- src/main/java/synapps/resona/api/mysql/social_media/entity/Likes.java
+++ src/main/java/synapps/resona/api/mysql/social_media/entity/Likes.java
@@ -1,22 +1,19 @@
 package synapps.resona.api.mysql.social_media.entity;
 
 import jakarta.persistence.*;
-import jakarta.validation.constraints.NotNull;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import synapps.resona.api.global.entity.BaseEntity;
 import synapps.resona.api.mysql.member.entity.member.Member;
 
-import java.time.LocalDateTime;
-
 @Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class Like extends BaseEntity {
+public class Likes extends BaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "like_id")
+    @Column(name = "likes_id")
     private Long id;
 
     // 멤버가 좋아요를 어디에 했는지 알 필요가 없어서 단방향 연관관계를 하려고 함
@@ -28,13 +25,13 @@ public class Like extends BaseEntity {
     @JoinColumn(name = "feed_id")
     private Feed feed;
 
-    private Like(Member member, Feed feed) {
+    private Likes(Member member, Feed feed) {
         this.member = member;
         this.feed = feed;
     }
 
-    public static Like of(Member member, Feed feed) {
-        return new Like(member, feed);
+    public static Likes of(Member member, Feed feed) {
+        return new Likes(member, feed);
     }
 
 }
