03406916a2742dc782cea83ab38c18a60b5ababf
speculatingwook <bwook9908@gmail.com>
2025-04-10 13:44:56 +0900

    feat: add feed complaint handling with service, repository, and controller.

---
+++ src/main/java/synapps/resona/api/mysql/socialMedia/controller/FeedComplaintsController.java
@@ -0,0 +1,37 @@
+package synapps.resona.api.mysql.socialMedia.controller;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import synapps.resona.api.global.config.server.ServerInfoConfig;
+import synapps.resona.api.global.dto.metadata.MetaDataDto;
+import synapps.resona.api.global.dto.response.ResponseDto;
+import synapps.resona.api.mysql.socialMedia.dto.feedComplaint.FeedComplaintRequest;
+import synapps.resona.api.mysql.socialMedia.entity.feedComplaint.FeedComplaint;
+import synapps.resona.api.mysql.socialMedia.service.FeedComplaintService;
+
+import java.util.List;
+
+@RestController
+@RequestMapping
+@RequiredArgsConstructor
+public class FeedComplaintsController {
+    private final FeedComplaintService feedComplaintService;
+    private final ServerInfoConfig serverInfo;
+
+    private MetaDataDto createSuccessMetaData(String queryString) {
+        return MetaDataDto.createSuccessMetaData(queryString, serverInfo.getApiVersion(), serverInfo.getServerName());
+    }
+
+    @PostMapping("/feed/{feedId}/complaint")
+    public ResponseEntity<?> reportFeed(HttpServletRequest request,
+                                        @PathVariable Long feedId,
+                                        @Valid @RequestBody FeedComplaintRequest complaintRequest) {
+        MetaDataDto metaData = createSuccessMetaData(request.getQueryString());
+        FeedComplaint complaint = feedComplaintService.reportFeed(feedId, complaintRequest);
+        ResponseDto responseData = new ResponseDto(metaData, List.of("reported"));
+        return ResponseEntity.ok(responseData);
+    }
+}
