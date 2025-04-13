f9e1b6f88ba753278a766c111fe95bd9d4394f55
speculatingwook <bwook9908@gmail.com>
2025-04-13 14:23:12 +0900

    refactor: refactor OAuth exception handling for provider mismatch scenarios

---
+++ src/main/java/synapps/resona/api/oauth/exception/OAuthException.java
@@ -0,0 +1,27 @@
+package synapps.resona.api.oauth.exception;
+
+import org.springframework.http.HttpStatus;
+import synapps.resona.api.global.exception.BaseException;
+import synapps.resona.api.global.exception.ErrorCode;
+import synapps.resona.api.mysql.member.entity.account.AccountInfo;
+import synapps.resona.api.oauth.entity.ProviderType;
+
+public class OAuthException extends BaseException {
+    protected OAuthException(String message, HttpStatus status, String errorCode) {
+        super(message, status, errorCode);
+    }
+
+    private static OAuthException of(ErrorCode errorCode) {
+        return new OAuthException(errorCode.getMessage(), errorCode.getStatus(), errorCode.getCode());
+    }
+
+    private static OAuthException of(ErrorCode errorCode, String customMessage) {
+        return new OAuthException(customMessage, errorCode.getStatus(), errorCode.getCode());
+    }
+
+    public static OAuthException OAuthProviderMissMatch(ProviderType providerType) {
+        String message = "Looks like you're signed up with wrong account. Please use your " + providerType + " account to login.";
+
+        return of(ErrorCode.PROVIDER_TYPE_MISSMATCH, message);
+    }
+}
