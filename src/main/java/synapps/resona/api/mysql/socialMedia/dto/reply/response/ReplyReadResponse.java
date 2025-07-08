e58161f2cc758c48a307de1280e4574afd8e6c0f
speculatingwook <bwook9908@gmail.com>
2025-07-08 16:45:30 +0900

    refactor: streamline ReplyResponse creation using builder methods.

--- src/main/java/synapps/resona/api/mysql/socialMedia/dto/reply/response/ReplyReadResponse.java
+++ src/main/java/synapps/resona/api/mysql/socialMedia/dto/reply/response/ReplyReadResponse.java
@@ -3,25 +3,27 @@ package synapps.resona.api.mysql.socialMedia.dto.reply.response;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import java.time.LocalDateTime;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import synapps.resona.api.mysql.socialMedia.entity.comment.Reply;
 
 @Data
-@NoArgsConstructor
-@AllArgsConstructor
+@Builder
 public class ReplyReadResponse {
 
-  private Long commentId;
-  private Long replyId;
+  private String commentId;
+  private String replyId;
   private String content;
 
   @JsonFormat(pattern = "yyyy-MM-dd")
   private LocalDateTime createdAt;
 
-  public ReplyReadResponse(Reply reply, Long commentId) {
-    this.commentId = commentId;
-    this.replyId = reply.getId();
-    this.content = reply.getContent();
+  public static ReplyReadResponse from(Reply reply) {
+    return ReplyReadResponse.builder()
+        .commentId(reply.getComment().getId().toString())
+        .replyId(reply.getId().toString())
+        .content(reply.getContent())
+        .build();
   }
 }
