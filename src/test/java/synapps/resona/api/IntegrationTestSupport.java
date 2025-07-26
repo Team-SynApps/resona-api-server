0bbff8b7a23c174ef4cf3b7bccef0836a4412650
speculatingwook <bwook9908@gmail.com>
2025-07-26 21:21:50 +0900

    cicd: add exclusions for auto-configurations in IntegrationTestSupport.

--- src/test/java/synapps/resona/api/IntegrationTestSupport.java
+++ src/test/java/synapps/resona/api/IntegrationTestSupport.java
@@ -1,10 +1,21 @@
 package synapps.resona.api;
 
 import org.junit.jupiter.api.TestInstance;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfiguration;
+import org.springframework.boot.autoconfigure.data.redis.RedisAutoConfiguration;
+import org.springframework.boot.autoconfigure.data.redis.RedisRepositoriesAutoConfiguration;
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
+@EnableAutoConfiguration(exclude = {
+    MongoAutoConfiguration.class,
+    MongoDataAutoConfiguration.class,
+    RedisAutoConfiguration.class,
+    RedisRepositoriesAutoConfiguration.class
+})
 public abstract class IntegrationTestSupport {
 
 }
