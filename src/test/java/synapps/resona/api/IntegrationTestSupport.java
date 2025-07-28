f0ca5eb55816b8960234e40e782798aadbce8284
speculatingwook <bwook9908@gmail.com>
2025-07-28 09:35:43 +0900

    cicd: update test config and streamline build process

--- src/test/java/synapps/resona/api/IntegrationTestSupport.java
+++ src/test/java/synapps/resona/api/IntegrationTestSupport.java
@@ -1,23 +1,20 @@
 package synapps.resona.api;
 
+import com.google.firebase.FirebaseApp;
 import org.junit.jupiter.api.TestInstance;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfiguration;
-import org.springframework.boot.autoconfigure.data.redis.RedisAutoConfiguration;
-import org.springframework.boot.autoconfigure.data.redis.RedisRepositoriesAutoConfiguration;
-import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.ActiveProfiles;
+import synapps.resona.api.global.config.FcmConfig;
 
-@SpringBootTest
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
-@EnableAutoConfiguration(exclude = {
-    MongoAutoConfiguration.class,
-    MongoDataAutoConfiguration.class,
-    RedisAutoConfiguration.class,
-    RedisRepositoriesAutoConfiguration.class
-})
 @ActiveProfiles("test")
+@SpringBootTest
 public abstract class IntegrationTestSupport {
 
+  @MockBean
+  private FcmConfig fcmConfig;
+
+  @MockBean
+  private FirebaseApp firebaseApp;
 }
--- src/test/java/synapps/resona/api/ResonaAPIServerTests.java
+++ src/test/java/synapps/resona/api/ResonaAPIServerTests.java
@@ -1,10 +1,21 @@
 package synapps.resona.api;
 
+import com.google.firebase.FirebaseApp;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.ActiveProfiles;
+import synapps.resona.api.global.config.FcmConfig;
 
+@ActiveProfiles("test")
 @SpringBootTest
 class ResonaAPIServerTests {
+
+  @MockBean
+  private FcmConfig fcmConfig;
+
+  @MockBean
+  private FirebaseApp firebaseApp;
 
   @Test
   void contextLoads() {
